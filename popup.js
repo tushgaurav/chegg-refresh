6fc34f901c01525441a12f9552ee51931b371117
Tushar Gaurav <59288386+tushgaurav@users.noreply.github.com>
2023-07-11 19:52:09 +0530

    basic functionality added

--- popup.js
+++ popup.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
   var durationInput = document.getElementById("duration");
   var saveButton = document.getElementById("save");
+  var stopButton = document.getElementById("stop");
 
   saveButton.addEventListener("click", function() {
     var duration = parseInt(durationInput.value, 10);
-    chrome.storage.sync.set({ refreshDuration: duration }, function() {
-      console.log("Refresh duration saved: " + duration);
-      window.close();
-    });
+    console.log(duration);
+    chrome.storage.sync.set({ duration  });
   });
+
+  stopButton.addEventListener("click", function () {
+    durationInput.value = 0;
+    var duration = parseInt(durationInput.value, 10);
+    console.log(duration);
+    chrome.storage.sync.set({ duration });
+  });
+
+  async function updateInput() {
+    const refreshDuration = await chrome.storage.sync.get("duration")
+    durationInput.value = refreshDuration.duration;
+  }
+
+  updateInput();
 });
--- scripts/content.js
+++ scripts/content.js
@@ -1,7 +1,51 @@
-console.warn("Chegg Refresh Script Loaded")
-
-while (true) {
-    setTimeout(() => {
-        location.reload();
-    }, 10000);
+// page reload function
+async function init() {
+  const refreshDuration = await chrome.storage.sync.get("duration");
+  return refreshDuration.duration;
 }
+
+let refreshDuration = null;
+
+init().then((duration) => {
+  refreshDuration = duration;
+  console.log("Refresh Duration: ", refreshDuration);
+  if (refreshDuration > 0) {
+    interval = pageReload(refreshDuration);
+  }
+  else {
+    stopPageReload(interval);
+  }
+});
+
+let interval = null;
+
+function pageReload(refreshDuration) {
+  console.log("Starting Page Reload");
+  let interval = setInterval(() => {
+    console.log("Reloading page");
+    window.location.reload();
+  }, refreshDuration * 1000);
+  return interval;
+}
+
+function stopPageReload(interval, startNew = false) {
+  if (interval) {
+    console.log("Stopping Page Reload");
+    clearInterval(interval);
+  } else {
+    console.log("No page reload to stop");
+  }
+}
+
+chrome.storage.onChanged.addListener((changes, area) => {
+  if (area === "sync" && changes.duration?.newValue) {
+    const newValue = changes.duration.newValue;
+    console.log("Old Refesh Duration: ", changes.duration.oldValue);
+    console.log("New Refesh Duration: ", newValue);
+    
+    refreshDuration = newValue;
+    if (changes.duration.oldValue == 0 && newValue > 0) {
+      window.location.reload();
+    }
+  }
+});
